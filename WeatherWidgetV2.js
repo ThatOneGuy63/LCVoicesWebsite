a524e0d2ed1bb7410bd9e7c717a21517ca774ac6
ThatOneGuy63 <Seby@algirdo.com>
2023-09-15 12:35:00 -0400

    Add hyperlink

--- WeatherWidgetV2.js
+++ WeatherWidgetV2.js
@@ -2,12 +2,13 @@ const accessToken = '7334a45d-c1b8-42fe-b9d5-ca471aa71258';
 const stationID = 118392;
 const conditionsUrl = `https://swd.weatherflow.com/swd/rest/better_forecast?station_id=${stationID}&units_temp=f&units_wind=mps&units_pressure=mb&units_precip=mm&units_distance=km&api_key=${accessToken}`;
 const temperatureUrl = `https://swd.weatherflow.com/swd/rest/observations/stn/${stationID}?bucket=1&ob_fields=air_temp&units_temp=f&units_wind=mps&units_pressure=mb&units_precip=mm&units_distance=km&api_key=${accessToken}`;
+const tempestDisplay = 'https://tempestwx.com/station/118392/grid';
 const options = { method: 'GET', headers: { accept: 'application/json' } };
 
 // Function to update the HTML widget with weather data
 function updateWeatherWidget(temperature, conditions) {
   const weatherWidget = document.getElementById('weather-widget');
-  weatherWidget.innerHTML = `${temperature} °F & ${conditions}<br>At Landmark College`;
+  weatherWidget.innerHTML = `<a href="${tempestDisplay}" target="_blank">${temperature} °F & ${conditions}<br>At Landmark College</a>`;
 }
 
 // Fetch both weather data and update the widget when both requests are complete
