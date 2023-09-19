d6319a8d66c8685c714677748398ec7a4a20d550
ThatOneGuy63 <Seby@algirdo.com>
2023-09-19 13:06:42 -0400

    sync commit

--- WeatherWidgetV2.js
+++ WeatherWidgetV2.js
@@ -2,7 +2,7 @@ const accessToken = '7334a45d-c1b8-42fe-b9d5-ca471aa71258';
 const stationID = 118392;
 const conditionsUrl = `https://swd.weatherflow.com/swd/rest/better_forecast?station_id=${stationID}&units_temp=f&units_wind=mps&units_pressure=mb&units_precip=mm&units_distance=km&api_key=${accessToken}`;
 const temperatureUrl = `https://swd.weatherflow.com/swd/rest/observations/stn/${stationID}?bucket=1&ob_fields=air_temp&units_temp=f&units_wind=mps&units_pressure=mb&units_precip=mm&units_distance=km&api_key=${accessToken}`;
-const tempestDisplay = 'https://tempestwx.com/station/118392/grid';
+const tempestDisplay = 'https://tempestwx.com/station/118392';
 const options = { method: 'GET', headers: { accept: 'application/json' } };
 
 // Function to update the HTML widget with weather data
